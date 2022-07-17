0e47d561adfd8714d2e3a973b818078f9c746bb5
Kira Kobzareva <kirakobzareva@gmail.com>
2022-07-17 14:32:18 +0200

    add creating form with reactive approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'forms-reactive-start';
+  genders = ['male', 'female'];
+  signupForm: FormGroup = new FormGroup({
+    username: new FormControl(null),
+    email: new FormControl(null),
+    gender: new FormControl('female'),
+  });
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
 }
