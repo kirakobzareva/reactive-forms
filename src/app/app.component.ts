59d951cc13f71e0b3f91ebe748da395f7c867032
Kira Kobzareva <kirakobzareva@gmail.com>
2022-07-17 14:48:43 +0200

    add validators to form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +9,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AppComponent {
   genders = ['male', 'female'];
   signupForm: FormGroup = new FormGroup({
-    username: new FormControl(null),
-    email: new FormControl(null),
+    username: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
     gender: new FormControl('female'),
   });
 
